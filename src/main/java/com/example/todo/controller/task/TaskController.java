98825b8697f448158d4df95125a496dede76cf8b
sungyo <hchengjian1@gmail.com>
2025-11-19 14:28:37 +0900

    TaskListを表示させるようにした

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -3,12 +3,18 @@ package com.example.todo.controller.task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 public class TaskController {
     @GetMapping("/tasks")
     public String list(Model model) {
-        model.addAttribute("test", "渡したい文字列");
+        List<TaskDTO> tasks = List.of(
+                new TaskDTO(1, "買い物", "スーパーで野菜を買う", "TODO"),
+                new TaskDTO(2, "勉強", "Spring Boot を学ぶ", "IN_PROGRESS"),
+                new TaskDTO(3, "運動", "ジョギング 5km", "DONE")
+        );
+        model.addAttribute("tasks", tasks);
         return "tasks/list";
     }
 }
